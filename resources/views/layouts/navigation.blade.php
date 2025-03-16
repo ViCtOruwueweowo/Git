309fc823512b3e14c888f6a1bdf6b9a555361eae
Victor_99 <vamonarrez21@gmail.com>
2025-03-16 17:30:58 -0600

    me vengo

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,154 @@
+<!-- Sidebar -->
+<div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="info">
+            <a href="{{ route('profile.show') }}" class="d-block">{{ Auth::user()->name }}</a>
+        </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+            data-accordion="false">
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                       Actores
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                       Direccion
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                       Categoria
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                       Ciudad
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                       Pais
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                       Cliente
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                        Filmes
+                        <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview" style="display: none;">
+                    <!-- Películas (Film) -->
+                    <li class="nav-item">
+                        <a href="..." class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Filmes</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="..." class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Filmes Actores</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="..." class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Filmes Categoria</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="..." class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Filmes Texto</p>
+                        </a>
+                    </li>
+                </ul>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                           Inventario
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                           Lenguaje
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                           Pago
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                           Alquiler
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                           Personal
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                           Almacen
+                        </p>
+                    </a>
+                </li>
+            </li>
+        </ul>
+    </nav>
+    
+    <!-- /.sidebar-menu -->
+</div>
+<!-- /.sidebar -->
